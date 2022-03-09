8cec990c4955b9b633e38d6ffc99ba1cac0f5d0c
ThomasStenholtLaursen <201905236@post.au.dk>
2022-03-09 14:33:40 +0100

    Add project files.

---
+++ ChargingCabinet.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChargingCabinet.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ChargingCabinet.Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChargingCabinet.Library
+{
+    public class Class1
+    {
+    }
+}
