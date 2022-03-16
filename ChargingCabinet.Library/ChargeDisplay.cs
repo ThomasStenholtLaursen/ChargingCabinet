354c25d9494502436291a7cd8198b68598ffd791
Samuel Lund Jessen <au646967@uni.au.dk>
2022-03-16 14:57:42 +0100

    Added 2 displays and IDisplay interface.

---
+++ ChargingCabinet.Library/ChargeDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingCabinet.Library
+{
+	class ChargeDisplay : IDisplay
+	{
+		
+		public void Print(string printString)
+		{
+
+			Console.WriteLine(printString);
+		}
+	}
+}
---
+++ ChargingCabinet.Library/InstructionDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargingCabinet.Library
+{
+	class InstructionDisplay : IDisplay
+	{
+		public void Print(string printString)
+		{
+			Console.WriteLine(printString);
+		}
+	}
+}
