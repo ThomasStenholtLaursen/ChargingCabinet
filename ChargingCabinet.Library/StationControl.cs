82c5ca4d9e8b97eb78b3f240c6f20385cdfce325
Rasmus Vorup <au676744@uni.au.dk>
2022-03-16 14:48:05 +0100

    Added RFID interface + class

--- ChargingCabinet.Library/StationControl.cs
+++ ChargingCabinet.Library/StationControl.cs
@@ -26,6 +26,10 @@ namespace ChargingCabinet.Library
         private string logFile = "logfile.txt"; // Navnet på systemets log-fil
 
         // Her mangler constructor
+        public StationControl()
+        {
+            //Test-constructor
+        }
 
         // Eksempel på event handler for eventet "RFID Detected" fra tilstandsdiagrammet for klassen
         private void RfidDetected(int id)
