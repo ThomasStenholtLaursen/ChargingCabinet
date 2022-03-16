e75ebc94fca2e273275ed9c3dea117367eb4be9b
Johan Andreas Balle Rubak <au662213@uni.au.dk>
2022-03-16 15:15:01 +0100

    Changes to RFID, subject side of observer pattern.

--- ChargingCabinet.Library/IRFIDReader.cs
+++ ChargingCabinet.Library/IRFIDReader.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+public class RFIDEventArgs : EventArgs
+{
+   public double Detected { set; get; }
+
+}
 
 public interface IRFIDReader
 {
+   public event EventHandler<RFIDEventArgs> RFIDDetectedEvent;
 
 }
--- ChargingCabinet.Library/RFIDReader.cs
+++ ChargingCabinet.Library/RFIDReader.cs
@@ -1,7 +1,16 @@
 ﻿using System;
 
-public class RFIDReader
+public class RFIDReader: IRFIDReader
 {
+   public event EventHandler<RFIDEventArgs> RFIDDetectedEvent;
 
+   protected virtual void OnDetection(RFIDEventArgs e)
+   {
+      RFIDDetectedEvent?.Invoke(this,e);
+   }
 
+   public void SetRFID(double rfid)
+   {
+      OnDetection(new RFIDEventArgs() {Detected = rfid});
+   }
 }
