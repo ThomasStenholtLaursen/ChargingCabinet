b73309dc7f4121e0947b86ce81c30bd40520b2be
ThomasStenholtLaursen <201905236@post.au.dk>
2022-03-17 13:08:09 +0100

    Started writing tests for ChargeControl

---
+++ ChargingCabinet.Test/ChargeControlTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using ChargingCabinet.Library;
+using NSubstitute;
+
+namespace ChargingCabinet.Test
+{
+    public class ChargeControlTest
+    {
+        private IChargeControl _uut;
+        private IUsbCharger _usbCharger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _usbCharger = Substitute.For<IUsbCharger>();
+            _uut = new ChargeControl(_usbCharger);
+        }
+
+        [TestCase(true,true)]
+        [TestCase(false, false)]
+        public void IsConnected_Method_TestsCorrectReturn(bool input,bool output)
+        {
+            _uut.Connected = input;
+            Assert.That(_uut.Connected, Is.EqualTo(output));
+        }
+
+        [Test]
+        public void StopChargeTest_CallsUsbChargerCorrectly()
+        {
+            _uut.StopCharge();
+            _usbCharger.Received(1).StopCharge();
+        }
+
+        [Test]
+        public void StartChargeTest_CallsUsbChargerCorrectly()
+        {
+            _uut.StartCharge();
+            _usbCharger.Received(1).StartCharge();
+        }
+    }
+}
---
+++ ChargingCabinet.Test/TestChargeControl.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace ChargingCabinet.Test
+{
+    public class Testsone
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
